04be2578a1386ef56dac4052b0cf3150a5f61f87
Trevin Shu <trevinshu2008@gmail.com>
2022-10-01 22:48:49 -0600

    updated api key

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -5,9 +5,10 @@ import { motion } from 'framer-motion';
 function SearchResult() {
   const [searchedWeather, setSearchedWeather] = useState([]);
   const params = useParams();
+  const apiKey = import.meta.env.VITE_API_KEY;
 
   const searchResult = async (query) => {
-    const data = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=rmFIiitZBSNgbQnlbsz0qADEb6N4FiXd&q=${query}`);
+    const data = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${query}`);
     const results = await data.json();
     setSearchedWeather(results.slice(0, 5));
   };
@@ -19,11 +20,13 @@ function SearchResult() {
   console.log(searchedWeather);
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="mt-5 px-5 md:px-10">
-      <h1 className=" text-2xl capitalize">Search Result For: {params.result}</h1>
-      <div className="grid grid-cols-2 gap-5 mt-5 md:grid-cols-4">
+      <h1 className=" text-2xl capitalize">
+        Search Result For: <span className="font-bold">{params.result}</span>
+      </h1>
+      <div className="grid grid-cols-2 gap-7 mt-5 md:grid-cols-4">
         {searchedWeather.map((weather, index) => {
           return (
-            <div key={index} className="shadow-sm p-4 border-red-600 border-4 flex flex-col items-center gap-2">
+            <div key={index} className="shadow-md p-4 flex flex-col items-center gap-3">
               <h2>
                 <span className="font-bold text-lg">Location: </span> {weather.LocalizedName}
               </h2>
